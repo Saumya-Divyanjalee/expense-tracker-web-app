cb2c0b3781c9b4b2ae49b2f85f77e7e2069dbf7c
saumy <saumyadivyanjalee529@gmail.com>
2026-09-18 11:42:18 +0530

    updated main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule, ObserveInstrument } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    instrument: ObserveInstrument,
-  });
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  await app.listen(process.env.PORT || 5000);
 }
-await bootstrap();
+bootstrap();
