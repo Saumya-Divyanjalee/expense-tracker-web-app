0fb41c0a681d973e741575c4fa47136953bd614a
saumy <saumyadivyanjalee529@gmail.com>
2026-09-18 11:38:51 +0530

    updated expenses-module

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ExpenseSchema } from './expense.schema';
-import { ExpensesService } from './expenses.service';
-import { ExpensesController } from './expenses.controller';
+import { PassportModule } from '@nestjs/passport';
+import { ExpenseSchema } from './expense.schema.js';
+import { ExpensesService } from './expenses.service.js';
+import { ExpensesController } from './expenses.controller.js';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Expense', schema: ExpenseSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Expense', schema: ExpenseSchema }]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [ExpensesController],
   providers: [ExpensesService],
 })
