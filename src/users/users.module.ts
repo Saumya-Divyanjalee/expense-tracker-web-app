f5134d6c3c160f28b097eddb12aaeb545dfa418c
saumy <saumyadivyanjalee529@gmail.com>
2026-09-18 11:45:13 +0530

    updated users-module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './user.schema';
-import { UsersService } from './users.service';
-
+import { UserSchema } from './user.schema.js';
+import { UsersService } from './users.service.js';
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UsersService],
