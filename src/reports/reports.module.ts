eac185d8923cc754cefc5bca167dfa8e4141d766
saumy <saumyadivyanjalee529@gmail.com>
2026-09-18 11:43:30 +0530

    updated reports-module

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ReportsController } from './reports.controller.js';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { ExpenseSchema } from '../expenses/expense.schema.js';
+import { IncomeSchema } from '../income/income.schema.js';
 import { ReportsService } from './reports.service.js';
+import { ReportsController } from './reports.controller.js';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Expense', schema: ExpenseSchema },
+      { name: 'Income', schema: IncomeSchema },
+    ]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
 })
 export class ReportsModule {}
