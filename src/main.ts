12e3df308ec3162eb0ecbb27a2e188be0be5a3d6
saumy <saumyadivyanjalee529@gmail.com>
2026-09-18 14:27:34 +0530

    deployed railway

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({ origin: '*', credentials: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT || 5000);
 }
